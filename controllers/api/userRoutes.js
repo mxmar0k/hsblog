4db9dd93583294d693a9d84e59b34e8f91503dde
Hola Bart <marcoferrusquia@gmail.com>
2023-09-06 22:52:25 -0600

    still on routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,54 +1,72 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-router.post('/', async (req, res) => {
+// Reusable error handling middleware
+const handleErrorResponse = (res, err) => {
+  console.error(err);
+  res.status(500).json({ error: "An error occurred" });
+};
+
+// we first get all users
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      handleErrorResponse(res, err);
+    });
+});
+
+// next we create a new user
+router.post("/", async (req, res) => {
   try {
     const userData = await User.create(req.body);
 
+    // we create a session for the new user
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-
       res.status(200).json(userData);
     });
   } catch (err) {
-    res.status(400).json(err);
+    handleErrorResponse(res, err);
   }
 });
 
-router.post('/login', async (req, res) => {
+// to login a user
+router.post("/login", async (req, res) => {
   try {
+    // we find the user by email
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+      res.status(400).json({ message: "Incorrect email or password, check credentials" });
       return;
     }
 
+    // we check if the password is valid
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+      res.status(400).json({ message: "Incorrect email or password, check credentials" });
       return;
     }
 
+    // Create a session for the logged-in user
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
+      res.status(200).json({ user: userData, message: "You have successfully logged in!" });
     });
-
   } catch (err) {
-    res.status(400).json(err);
+    handleErrorResponse(res, err);
   }
 });
 
-router.post('/logout', (req, res) => {
+// then we logout a user with destroy session
+router.post("/logout", withAuth, (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
