1cc1e63b4b05ebe2e1936793d39b9a713b606401
Hola Bart <marcoferrusquia@gmail.com>
2023-09-09 15:27:37 -0600

    help

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const handlebars = expressHandlebars.create({helpers:require("./Utils/helpers")}
 
 //express app and port
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 //session with a cookie and store in sequelizestore
 const sess = {
